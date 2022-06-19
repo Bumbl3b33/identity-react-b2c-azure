03c65b73f3ae487475ab067ed4d9c092341a1e99
Azam Fahmy <azamf@infyinnos.com>
2022-06-20 00:24:15 +0530

    Add boostrap styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
 import React, { useState } from 'react';
+import "./Home.css"
 
 function Home (){
 
@@ -25,22 +26,31 @@ function Home (){
     }
 
     return (
-        <div>
-            <h1>Homepage</h1>
+        <div className="home"> 
+                <p class="text-center fs-5 fw-bold">
+                    Authenticating a React App using Azure AD B2C
+                </p>
             <AuthenticatedTemplate>
-                <button onClick={() => Logout()}>Logout</button>
-                <div>
-                    You're authenticated!
-                </div>
-                <div>
-                    {idToken}
+                <div class="alert alert-success" role="alert">
+                    You are authenticated! 😊
+                    <button type="button" class="btn btn-dark btn-sm float-end" onClick={() => Logout()}>Logout</button>
+                </div>  
+                
+                <div class="card">
+                    <div class="card-header">
+                        Id Token
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">{idToken}</li>
+                        <li class="list-group-item">Paste the above on <span ><a href="https://jwt.ms" target="_blank ">jwt.ms</a></span> to decode the token</li>
+                    </ul>
                 </div>
             </AuthenticatedTemplate>
             <UnauthenticatedTemplate>
-                <div>
-                    Why you no authenticate?
-                </div>
-                <button onClick={() => Login()}>Login</button>
+                <div class="alert alert-warning" role="alert">
+                    You are not authenticated 🥺
+                    <button type="button" class="btn btn-dark btn-sm float-end" onClick={() => Login()}>Login</button>
+                </div>  
             </UnauthenticatedTemplate>
         </div>
     )
